7edab8a43acf04ef875c20b98bfdea6ef41fa426
mahekagg <105991553+mahekagg@users.noreply.github.com>
2023-01-30 16:21:04 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+// JS File
+// Ab Mosca 
+// Last moditifed: 01.30.2023 
+
+function onClick(){
+    count++
+    document.getElementById("button-div").innerHTML = count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
